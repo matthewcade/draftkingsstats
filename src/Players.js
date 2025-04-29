a273c4cee5b3c9b9707493e62c2ebd71f042e2ec
Cade Hancock <93878207+matthewcade@users.noreply.github.com>
2025-04-28 22:57:29 -0500

    Styled Player Backgrounds
    Player card backgrounds alternate between white and grey

--- src/Players.js
+++ src/Players.js
@@ -24,6 +24,14 @@ const database = [
         7200, 
         23.2, 
         77
+    ],
+    [4,
+        'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3916387.png',
+        'Lamar Jackson',
+        'QB',
+        7000,
+        23.9,
+        69
     ]
 ];
 
@@ -65,32 +73,169 @@ export function PlayerComponent() {
         });
     }
 
-    //Buttons to sort the players
+    function sortAZ(item, arr) {
+        setPlayers(() => {
+            let sortPlayers = [...arr];
+            sortPlayers.sort((a, b) => (a[item] < b[item] ? -1 : a[item] > b[item] ? 1 : 0));
+
+            if ((currentSort.length == 0) || (currentSort == database) || (currentSort !== database && JSON.stringify(sortPlayers) !== JSON.stringify(currentSort))) {
+                currentSort = sortPlayers;
+                return sortPlayers;
+            } else {
+                currentSort = database;
+                return database;
+            }
+        })
+    }
+
+    function sortZA(item, arr) {
+        setPlayers(() => {
+            let sortPlayers = [...arr];
+            sortPlayers.sort((a, b) => (a[item] === b[item] ? 0 : a[item] < b[item] ? 1 : -1));
+
+            if ((currentSort.length == 0) || (currentSort == database) || (currentSort !== database && JSON.stringify(sortPlayers) !== JSON.stringify(currentSort))) {
+                currentSort = sortPlayers;
+                return sortPlayers;
+            } else {
+                currentSort = database;
+                return database;
+            }
+        })
+    }
+
+    //Buttons to sort the players by position
+    function PositionButtons() {
+        return (
+            <div className='sorting'>
+                <h2>Position</h2>
+                <div>
+                    <button className='sort-btns' onClick={() => sortAZ(3, players)}>^</button>
+                    <button className='sort-btns' onClick={() => sortZA(3, players)}>v</button>
+                </div>
+            </div>
+        )
+    }
+
+    //Buttons to sort the players by name
+    function NameButtons() {
+        return (
+            <div className='sorting'>
+                <h2>Name</h2>
+                <div>
+                    <button className='sort-btns' onClick={() => sortAZ(2, players)}>^</button>
+                    <button className='sort-btns' onClick={() => sortZA(2, players)}>v</button>
+                </div>
+            </div>
+        )
+    }
+
+    //Buttons to sort the players by cost
     function CostButtons() {
         return (
-            <div id='sort-cost-btns'>
-                <button className='sort-btns' onClick={() => sortHighLow(5, players)}>^</button>
-                <button className='sort-btns' onClick={() => sortLowHigh(5, players)}>v</button>
+            <div className='sorting'>
+                <h2>Cost</h2>
+                <div>
+                    <button className='sort-btns' onClick={() => sortHighLow(4, players)}>^</button>
+                    <button className='sort-btns' onClick={() => sortLowHigh(4, players)}>v</button>
+                </div>
+            </div>
+        )
+    }
+
+    //Buttons to sort the players by average points
+    function AvgButtons() {
+        return (
+            <div className='sorting'>
+                <h2>Avg Pts</h2>
+                <div>
+                    <button className='sort-btns' onClick={() => sortHighLow(5, players)}>^</button>
+                    <button className='sort-btns' onClick={() => sortLowHigh(5, players)}>v</button>
+                </div>
+            </div>
+        )
+    }
+
+    //Buttons to sort the players by opponent rating
+    function OppButtons() {
+        return (
+            <div className='sorting'>
+                <h2>Opp Rtg</h2>
+                <div>
+                    <button className='sort-btns' onClick={() => sortHighLow(6, players)}>^</button>
+                    <button className='sort-btns' onClick={() => sortLowHigh(6, players)}>v</button>
+                </div>
+            </div>
+        )
+    }
+
+    function PlayerCards() {
+        return (
+            <div id='player-wrapper'>
+                <h1>Players</h1>
+                <ul id='players'>{players.map((player) => (
+                    <li className='player-card' key={player[0]}>
+                        <img src={player[1]} key={`pic_${player[1]}`} alt={player[3]}  width='100px' height='75px' />
+                        <ul>
+                            <li key={`id=${player[0]}`}><strong>{player[3]}</strong> {player[2]}</li>
+                            <li key={`cost_${player[0]}`}>Cost: {player[4]}</li>
+                            <li key={`avg_${player[0]}`}>Avg.: {player[5]} pts.</li>
+                            <li key={`opprtg_${player[0]}`}>Opp. Rtg.: {player[6]}</li>
+                        </ul>
+                    </li>
+                ))}</ul>
+            </div>
+        )
+    }
+
+    function Lineup() {
+        return (
+            <div id='lineup'>
+                <ul>
+                    <li id='qb'>
+                        <li><strong>QB</strong></li>
+                    </li>
+                    <li id='rb1'>
+                        <li><strong>RB</strong></li>
+                    </li>
+                    <li id='rb2'>
+                        <li><strong>RB</strong></li>
+                    </li>
+                    <li id='wr1'>
+                        <li><strong>WR</strong></li>
+                    </li>
+                    <li id='wr2'>
+                        <li><strong>WR</strong></li>
+                    </li>
+                    <li id='te'>
+                        <li><strong>TE</strong></li>
+                    </li>
+                    <li id='flex'>
+                        <li><strong>FLEX</strong></li>
+                    </li>
+                    <li id='defense'>
+                        <li><strong>DEF</strong></li>
+                    </li>
+                    <li id='projection'>
+                        <li><strong>Projected Score: </strong></li>
+                    </li>
+                </ul>
             </div>
         )
     }
 
     return (
-        <div>
-            <h2>Sort Cost</h2>
-            <CostButtons />
-            <h1>Players</h1>
-            <ul>{players.map((player) => (
-                <li key={player[0]}>
-                    <img src={player[1]} key={`pic_${player[1]}`} alt={player[3]} width='100px' height='75px' />
-                    <ul>
-                        <li key={`id=${player[0]}`}><strong>{player[3]}</strong> {player[2]}</li>
-                        <li key={`cost_${player[0]}`}>Cost: {player[4]}</li>
-                        <li key={`avg_${player[0]}`}>Avg.: {player[5]} pts.</li>
-                        <li key={`opprtg_${player[0]}`}>Opp. Rtg.: {player[6]}</li>
-                    </ul>
-                </li>
-            ))}</ul>
+        <div id='wrapper'>
+            <div id='sorting'>
+                <PositionButtons />
+                <NameButtons />
+                <CostButtons />
+                <AvgButtons />
+                <OppButtons />
+            </div>
+            <div id='main'>
+                <PlayerCards />
+                <Lineup />
+            </div>
         </div>
     );
 }
